48b2cb9c58d58fee1711b65a1d0d559081ddbe27
Jeong-DongHyeon <dev.donghyeon@gmail.com>
2022-06-11 22:18:39 +0900

    Build: buildSrc 모듈 구현

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,49 @@
+
+object Libs {
+    const val appcompat =
+        "androidx.appcompat:appcompat:1.4.2"
+    const val core_ktx =
+        "androidx.core:core-ktx:1.8.0"
+    const val activity_ktx =
+        "androidx.activity:activity-ktx:1.4.0"
+    const val fragment_ktx =
+        "androidx.fragment:fragment-ktx:1.4.1"
+    const val lifecycle_viewmodel_ktx =
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1"
+    const val lifecycle_livedata_ktx =
+        "androidx.lifecycle:lifecycle-livedata-ktx:2.4.1"
+    const val datastore_preferences =
+        "androidx.datastore:datastore-preferences:1.0.0"
+    const val constraintlayout =
+        "androidx.constraintlayout:constraintlayout:2.1.3"
+    const val room_runtime =
+        "androidx.room:room-runtime:2.4.2"
+    const val room_compiler =
+        "androidx.room:room-compiler:2.4.2"
+    const val coroutines_core =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2"
+    const val coroutines_android =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1"
+    const val retrofit =
+        "com.squareup.retrofit2:retrofit:2.9.0"
+    const val converter_gson =
+        "com.squareup.retrofit2:converter-gson:2.9.0"
+    const val logging_interceptor =
+        "com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.5"
+    const val material =
+        "com.google.android.material:material:1.6.1"
+    const val hilt_android =
+        "com.google.dagger:hilt-android:2.42"
+    const val hilt_compiler =
+        "com.google.dagger:hilt-compiler:2.42"
+    const val timber =
+        "com.jakewharton.timber:timber:5.0.1"
+    const val junit =
+        "junit:junit:4.13.2"
+    const val mockk =
+        "io.mockk:mockk:1.12.4"
+    const val coroutines_test =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.2"
+    const val core_testing =
+        "androidx.arch.core:core-testing:2.1.0"
+}
